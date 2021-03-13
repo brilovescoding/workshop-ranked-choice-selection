06c3375db8b7dfa1b0fd92069b7238a130e209a1
Brian Schott <bschott@cgps.org>
2021-03-12 22:25:38 -0500

    starting method to import data (CSV data schema needed) Decided not to use serializable objects for this project because I am using factory pattern

--- src/main/java/EnrollmentManager.java
+++ src/main/java/EnrollmentManager.java
@@ -1,4 +1,7 @@
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
+
 import com.opencsv.*;
 
 //class will contain methods for importing attendance and workshop data
@@ -14,6 +17,27 @@ public class EnrollmentManager {
         completedWorkshopEnrollment = new ArrayList<Workshop>();
     }
 
+    //method takes in a filepath, imports the file into a List of String arrays,
+    // (one for each row), and changes each into a list of Workshops and Attendees
+    //Workshops will utilize the WorkshopFactory
+    public void importData(String workshopCSVFilePath, String attendeeCSVFilePath) {
+        List<String[]> workshopData = getListFromCSV(workshopCSVFilePath);
+        List<String[]> attendeeData = getListFromCSV(attendeeCSVFilePath);
+
+        //loop through attendee data, instantiate objects for each attendee
+
+        //loop through workshopData; instantiate objects for workshopList
+        //order of column in spreadsheet are:
+    }
+
+    public List<String[]> getListFromCSV(String filepath) {
+        try {
+            CSVReader reader = new CSVReader(new FileReader(filepath));
+            return reader.readAll();
+        } catch (Exception e) { System.out.println("Error reading in file."); }
+
+        return null;
+    }
 
 
 }
